cdb9a01d2e6a78d1bee2caf390a94ab6c6611fce
Егор Софронов <egor.sofronov03@gmail.com>
2021-02-17 14:25:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ConvertNS_0_2;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int at, bt;
+            string s;
+            Console.WriteLine ( "Введите число: ");
+            s=Console.ReadLine(); 
+            Console.WriteLine ( "Введите начальную систему: ");
+            at =Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine ( "Введите конечную систему: ");
+            bt=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ответ ");
+            if (at < 2 || at > 16 || bt < 2 || bt > 16) Console.WriteLine( "Введенные данные не удовлетворяют условию!");
+            else
+            { 
+                Console.WriteLine(ConvertNS_0_2.ConvertNS.Toany(at, bt, s));
+
+            }
+
+        }
+    }
+}
